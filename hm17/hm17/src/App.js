223b8983a55c286887751579d50160bb0a4a0b21
Aleksandr Racovets <57405165+alexracovets@users.noreply.github.com>
2023-05-15 05:58:31 +0300

    added home work 17

---
+++ hm17/hm17/src/App.js
@@ -0,0 +1,11 @@
+import Contacts from './components/Contacts/Contacts'
+
+function App() {
+  return (
+    <div className="App">
+      <Contacts></Contacts>
+    </div>
+  );
+}
+
+export default App;
